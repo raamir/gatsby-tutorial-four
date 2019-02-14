ef3265b36128364d3e83d6f1823460ee6891eb8c
aamir.rija <aamir.rija@gmail.com>
2019-02-14 14:41:06 -0500

    Tutorial Step 8

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 // import { css } from "@emotion/core"
 import { StaticQuery, Link, graphql } from "gatsby"
+import { Helmet } from "react-helmet"
 
 // import { rhythm } from "../utils/typography"
 
@@ -17,6 +18,11 @@ export default ({ children }) => (<StaticQuery
 }
     render={data => (
   <div style={{ margin: `0 auto`, maxWidth: `700px`, padding: `2%`, paddingTop: `1.5%` }}>
+         <Helmet>
+          <meta charSet="utf-8" />
+          <title>My Title</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
     <Link to={`/`}>
       <h3 style={{ marginBottom: `2%`, display: `inline-block`, fontStyle: `normal` }}>
          {data.site.siteMetadata.title}
