e761df7ef4c8cbec105ec29ebbcf43e471503e00
aamir.rija <aamir.rija@gmail.com>
2019-02-14 14:00:32 -0500

    Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,14 @@
 module.exports = {
   siteMetadata: {
     title: `Pandas Eating Lots`,
-  }
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+   ],
 }
